40876a0787598bfb47929b30f9230638f10b8544
levi <glendenning_levi@hotmail.com>
2018-07-11 22:14:09 -0600

    variable dirname for static assets

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
 
 // app.get("/api/user", (req, res) => {
