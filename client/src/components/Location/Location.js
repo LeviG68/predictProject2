5e71dc5cd682d710099af5815ddf9eaa33acc22a
Stephanie Glendenning <mtfishergrl@hotmail.com>
2018-07-18 20:27:19 -0600

    Sign In page done

--- client/src/components/Location/Location.js
+++ client/src/components/Location/Location.js
@@ -13,7 +13,7 @@ class Location extends React.Component {
         <div className = "address">
           <span>{this.props.address}</span>
         </div>
-        <a href={this.props.website} className="website">           Visit Site
+        <a href={this.props.website} className="website" target="_blank">Visit Site
         </a>
         <div className = "graph">
           <span>{this.props.graph}</span>
